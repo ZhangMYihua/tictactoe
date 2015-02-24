5270c8a71931bf093cdd777a2c2cc9a43086a4b9
zhangmyihua <michaelzhang2@msn.com>
2015-02-24 08:09:37 -0500

    Chris Gahan showed a very interesting solution involving two for loops that check through an array of arrays containing winning combinations. Interesting way to look at the solution since the css elements in .column will save with proper indices in an array

--- game.js
+++ game.js
@@ -19,46 +19,50 @@ $(document).ready(function(){
 		that.off('click')
 	});
 
-	var cells = $(".column")
+// Crazy method by Chris Gahan, very interesting to note that all the elements with class column save in an array 
+// with the proper index according to the grid layour allowing for the check to work. 
 
-	// [0 1 2]  
-	// [3 4 5]
-	// [6 7 8]
-	var winning_positions = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ]
+							// // [0 1 2]  
+							// // [3 4 5]
+							// // [6 7 8]
+							
+							// var cells = $(".column")
+							// var winning_positions = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ]
 
-	// winning_positions.any? { |triple| triple.all? { |n| cells[n].hasClass(player) } }
+							// // winning_positions.any? { |triple| triple.all? { |n| cells[n].hasClass(player) } }
 
-  var check_winner = function(player) {
-		for (var i = 0; i < winning_positions.length; i++) {
-			var row = winning_positions[i];
+						 //  var check_winner = function(player) {
+							// 	for (var i = 0; i < winning_positions.length; i++) {
+							// 		var row = winning_positions[i];
 
-			var winner = true;
+							// 		var winner = true;
 
-			for (var j = 0; j < row.length; j++) {
-				if (!$(cells[j]).hasClass(player))
-					winner = false;
-			}
+							// 		for (var j = 0; j < row.length; j++) {
+							// 			if (!$(cells[j]).hasClass(player))
+							// 				winner = false;
+							// 		}
 
-			if (winner)
-				return true;
-			
-		}
+							// 		if (winner)
+							// 			return true;
+									
+							// 	}
 
-		return false;
-	}
+							// 	return false;
+							// // }
 
 var checkIfWon = function(n){
-	// if (
-	// 	 ($('#grid1').hasClass(n) && $('#grid2').hasClass(n) && $('#grid3').hasClass(n)) ||
-	// 	 ($('#grid1').hasClass(n) && $('#grid4').hasClass(n) && $('#grid7').hasClass(n)) ||
-	// 	 ($('#grid2').hasClass(n) && $('#grid5').hasClass(n) && $('#grid8').hasClass(n)) ||
-	// 	 ($('#grid3').hasClass(n) && $('#grid6').hasClass(n) && $('#grid9').hasClass(n)) ||
-	// 	 ($('#grid1').hasClass(n) && $('#grid5').hasClass(n) && $('#grid9').hasClass(n)) ||
-	// 	 ($('#grid3').hasClass(n) && $('#grid5').hasClass(n) && $('#grid7').hasClass(n)) ||
-	// 	 ($('#grid4').hasClass(n) && $('#grid5').hasClass(n) && $('#grid6').hasClass(n)) ||
-	// 	 ($('#grid7').hasClass(n) && $('#grid8').hasClass(n) && $('#grid9').hasClass(n))
-	// 	)
-	if (check_winner(n)) {
+	if (
+		 ($('#grid1').hasClass(n) && $('#grid2').hasClass(n) && $('#grid3').hasClass(n)) ||
+		 ($('#grid1').hasClass(n) && $('#grid4').hasClass(n) && $('#grid7').hasClass(n)) ||
+		 ($('#grid2').hasClass(n) && $('#grid5').hasClass(n) && $('#grid8').hasClass(n)) ||
+		 ($('#grid3').hasClass(n) && $('#grid6').hasClass(n) && $('#grid9').hasClass(n)) ||
+		 ($('#grid1').hasClass(n) && $('#grid5').hasClass(n) && $('#grid9').hasClass(n)) ||
+		 ($('#grid3').hasClass(n) && $('#grid5').hasClass(n) && $('#grid7').hasClass(n)) ||
+		 ($('#grid4').hasClass(n) && $('#grid5').hasClass(n) && $('#grid6').hasClass(n)) ||
+		 ($('#grid7').hasClass(n) && $('#grid8').hasClass(n) && $('#grid9').hasClass(n))
+		)
+	// if (check_winner(n)) {
+		{
 		console.log(turn);
 		window.alert(n + " has won!!");
 		$('.column').off('click');
